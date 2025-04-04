931c33d87e18f364129daac5fb35d652747a7a70
Matt Radford <quinquex@gmail.com>
2025-04-04 10:14:37 -0700

    add user model

--- berries/main_app/models.py
+++ berries/main_app/models.py
@@ -1,6 +1,7 @@
 from re import M
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -24,6 +25,7 @@ class Berry(models.Model):
     variety = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     season = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.get_name_display()} ({self.id})'
