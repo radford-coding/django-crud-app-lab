5174abc4661a8caaf6c14193df949324ba5211b9
Matt Radford <quinquex@gmail.com>
2025-04-06 15:29:26 -0700

    tweak colors

--- berries/main_app/templatetags/color_calc.py
+++ berries/main_app/templatetags/color_calc.py
@@ -4,20 +4,7 @@ register = template.Library()
 
 @register.filter
 def color(value):
-    green = int(50 + sum(value.name.encode('ascii')) % 100)
-    red = int(sum(value.description.encode('ascii')) % (green / 2))
-    blue = int(sum(value.city.encode('ascii')) % (green / 2))
+    green = int(100 + (sum(value.name.encode('ascii')) % 100))
+    red = int(sum(value.description.encode('ascii')) % (green / 3))
+    blue = int(sum(value.city.encode('ascii')) % (green / 3))
     return '#{:02X}{:02X}{:02X}'.format(red, green, blue)
-    # return 90
-
-@register.filter
-def color_green(value):
-    return 30 + sum(value.name.encode('ascii')) % 120
-
-@register.filter
-def color_red(value):
-    return sum(value.description.encode('ascii')) % 30
-
-@register.filter
-def color_blue(value):
-    return sum(value.city.encode('ascii')) % 30
